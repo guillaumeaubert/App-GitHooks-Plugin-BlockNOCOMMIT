60615d825fa9627cb163c6971e99cca0699d85c4
Guillaume Aubert <aubertg@cpan.org>
2014-06-07 23:20:16 -0700

    Release version 1.0.2.

--- lib/App/GitHooks/Plugin/BlockNOCOMMIT.pm
+++ lib/App/GitHooks/Plugin/BlockNOCOMMIT.pm
@@ -49,11 +49,11 @@ Note that the following variations on the tag are correctly picked up:
 
 =head1 VERSION
 
-Version 1.0.1
+Version 1.0.2
 
 =cut
 
-our $VERSION = '1.0.1';
+our $VERSION = '1.0.2';
 
 
 =head1 METHODS
